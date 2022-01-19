dc2e9a819ec06da2eb86a18f7755d319dfc684b2
Cristian Carvajal <xsoultoy@hotmail.com>
2022-01-19 18:32:43 +0100

    Add project files.

---
+++ LibraryEmpleados/Empleados.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryEmpleados
+{
+    // Mapear nuestra base de datos
+    [Table("EMP")]
+    public class Empleados
+    {
+        [Key]
+        [Column("EMP_NO")]
+        public int IdEmpleado { get; set; }
+        [Column("APELLIDO")]
+        public String Apellido { get; set; }
+        [Column("OFICIO")]
+        public String Oficio { get; set; }
+        [Column("SALARIO")]
+        public int Salario { get; set; }
+        [Column("DEPT_NO")]
+        public String IdDepartamento { get; set; }
+    }
+}
